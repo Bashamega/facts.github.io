7b1e7258a7d2e0e60b822b958343315ad5be7746
Bashamega <adambashaahmednaji@gmail.com>
2022-08-23 12:39:15 +0200

    edit

---
+++ script.js
@@ -0,0 +1,41 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    const answer = confirm("This website is not fully functioning on small devices. Try opening this website on a computer. Press ok to close")
+    
+
+}
+
+
+/*--functions-- */
+function page1(){
+    window.open("50.html")
+}
+function home(){
+    window.open("index.html")
+}
+function Science(){
+    window.open("science.html")
+}
+function Hist(){
+    window.open("Hist.html")
+}
+function add(){
+    window.open("add.html")
+}
+function page2(){
+    window.open("51.html")
+}
+function sum(){
+    alert("Your post has been submitted.")
+}
+function page3(){
+    window.open("52.html")
+}
+function page4(){
+    window.open("53.html")
+}
+function page5(){
+    window.open("54.html")
+}
+function page6(){
+    window.open("55.html")
+}
